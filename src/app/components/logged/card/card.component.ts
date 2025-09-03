f72cff0e84709c8e2425867539208509fbafd4fa
Rafael955 <rafaelcaffonso@gmail.com>
2025-09-03 19:06:46 -0300

    Criando header e iniciando componente card para home logada

--- src/app/components/logged/card/card.component.ts
+++ src/app/components/logged/card/card.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() duration?: string;
+  @Input() ageRating?: string;
+  @Input() approval?: string;
+  @Input() top10: boolean = false;
+  @Input() serviceLogo?: string;
+  @Input() serviceName?: string;
 
+  ageRatingSymbol: any = "bi bi-exclamation-triangle";
+
+  ngOnInit(){
+    if(this.ageRating === 'Livre' || this.ageRating === '0 anos'){
+      this.ageRatingSymbol = "bi bi-emoji-smile";
+    } else if(this.ageRating === '12 anos' || this.ageRating === '14 anos'){
+      this.ageRatingSymbol = "bi bi-hand-thumbs-up";
+    } else if(this.ageRating === '16 anos' || this.ageRating === '+16') {
+      this.ageRatingSymbol = "bi bi-exclamation-circle";
+    } if(this.ageRating === '18 anos' || this.ageRating === '+18' || this.ageRating === 'Somente +18') {
+      this.ageRatingSymbol = "bi bi-exclamation-triangle";
+    }
+  }
 }
